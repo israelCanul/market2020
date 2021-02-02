9b3583655bbbfd5168f0a41e598adf8f1fa757c7
israel <israel@cloudware.mx>
2021-02-01 20:58:18 -0500

    npm update

--- src/App.js
+++ src/App.js
@@ -6,7 +6,7 @@ class App extends Component {
   render() {
     return (
       <div className="App">
-        <h1> Hello, Worldsssasqweqwe! </h1>
+        <h1> Hello, Worlde! </h1>
       </div>
     );
   }
