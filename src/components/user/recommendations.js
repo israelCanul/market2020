d6fe86f7f6b666316c71c4ee74ef635ac492f0e3
israel <israel@cloudware.mx>
2021-04-07 17:20:40 -0500

    recommended item based in the user´s history

--- src/components/user/recommendations.js
+++ src/components/user/recommendations.js
@@ -1,28 +1,47 @@
 import React from "react";
 import Item from "../Item";
 import getTexto from "../../libs/messages";
+import { Link } from "react-router-dom";
+import getURL from "../../libs/Routes";
+import { LazyLoadImage } from "react-lazy-load-image-component";
+import _ from "lodash";
 
 function Recommendations(props) {
   if (props.site.user) {
     if (props.site.user.itemsHistory) {
       if (props.site.user.itemsHistory.length > 0) {
+        let itemRandomFromHistory =
+          props.site.user.itemsHistory[
+            _.random(0, Object.keys(props.site.user.itemsHistory).length - 1)
+          ];
+        let itemsFromCat = new Array();
+        itemsFromCat = _.filter(props.state.storeItems, function (o) {
+          return (
+            o.Category.SCategoryCode ==
+            itemRandomFromHistory.Category.SCategoryCode
+          );
+        });
         return (
           <div className="section">
             <div className="section_content">
               <div className="information">
                 <h3>{getTexto("Recommendations for you")}</h3>
               </div>
-              <div className="img">
-                <img
-                  width="66"
-                  height="157"
-                  src="img/home/ejem_recommendations_for_you.jpg"
-                  alt="Ejemplo Item"
+              {itemsFromCat.length > 0 ? (
+                <Item
+                  recommendations
+                  item={
+                    itemsFromCat[
+                      _.random(
+                        0,
+                        Object.keys(props.site.user.itemsHistory).length - 1
+                      )
+                    ]
+                  }
                 />
-              </div>
-              <div className="action_link">
-                <a href="#">{getTexto("View More")}</a>
-              </div>
+              ) : (
+                <Item recommendations item={itemRandomFromHistory} />
+              )}
             </div>
           </div>
         );
